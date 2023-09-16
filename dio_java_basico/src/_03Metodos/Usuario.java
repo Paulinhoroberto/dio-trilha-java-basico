c3ac55315ad4ffa03f3ae8cfe8918ecfc5d038b4
Paulinhoroberto <pauloroberto_1994@outlook.com>
2023-09-15 21:02:19 -0300

    Metodos

---
+++ dio_java_basico/src/_03Metodos/Usuario.java
@@ -0,0 +1,31 @@
+package _03Metodos;
+
+public class Usuario {
+
+	public static void main(String[] args) {
+
+		SmartTv smartTv = new SmartTv();
+
+		smartTv.dimunirVolume();
+		smartTv.dimunirVolume();
+		smartTv.dimunirVolume();
+		smartTv.aumentarVolume();
+
+		System.out.println("Canal atual : " + smartTv.canal);
+		smartTv.mudarCanal(13);
+		System.out.println("Canal atual : " + smartTv.canal);
+
+		System.out.println("Volume atual : " + smartTv.volume);
+
+		System.out.println("TV ligada ? " + smartTv.ligada);
+		System.out.println("Canal atual : " + smartTv.canal);
+		System.out.println("Volume atual : " + smartTv.volume);
+
+		smartTv.ligar();
+		System.out.println(" Novo status, TV ligada ? " + smartTv.ligada);
+
+		smartTv.desligar();
+		System.out.println(" Novo status, TV ligada ? " + smartTv.ligada);
+	}
+
+}
